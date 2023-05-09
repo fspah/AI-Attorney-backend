1f1b54da0cfbb9dadeb8cfb9e1336581c67eb261
Frane <franespahija8@gmail.com>
2023-05-09 23:45:24 +0200

    added print

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ async def process_pdf(file: UploadFile = File(None),
 
 if __name__ == '__main__':
     import uvicorn
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000)) # default to 5000 if PORT is not set
+    print(f'Starting server on port {port}')
     uvicorn.run(app, host='0.0.0.0', port=port)
 
