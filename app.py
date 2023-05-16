15a3dcd8a4c75cbaa4dd9ff8288c31ac3c6263aa
Frane <franespahija8@gmail.com>
2023-05-16 17:02:58 +0200

    maybe

--- app.py
+++ app.py
@@ -60,7 +60,12 @@ def process_document_and_query(file):
 
     filename = os.path.basename(file)
     docsearch = Pinecone.from_texts(
-        [t.page_content for t in texts], embeddings, index_name=index_name, metadatas={"filename": filename})
+        [
+            t.page_content for t in texts],
+        embeddings,
+        index_name=index_name,
+        metadata={
+            "filename": filename})
     print('a', flush=True)
     print(docsearch, flush=True)
 
@@ -68,7 +73,10 @@ def process_document_and_query(file):
 
 
 def process_question(docsearch, question, prompt, filename):
-    docs = docsearch.similarity_search(question, include_metadata=True, filter={"filename": {"$eq": filename}})
+    docs = docsearch.similarity_search(
+        question, include_metadata=True, filter={
+            "filename": {
+                "$eq": filename}})
     print('a', flush=True)
 
     llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
