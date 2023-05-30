80f00080853940f2f9648957c22e06a1b8d276b3
Frane <franespahija8@gmail.com>
2023-05-30 17:52:34 +0200

    print

--- app.py
+++ app.py
@@ -156,7 +156,9 @@ async def upload_file(file: UploadFile = File(...)):
 async def process_pdf(chat: Chat = Body(...)):
     prompt = ''
     messages = [message.dict() for message in chat.messages]
-    last_message = messages[-1]
+    if messages:
+        last_message = messages[-1]
+    print(messages)
     question = last_message['content']
     # Convert messages to string
     all_but_last_messages = messages[:-1]
